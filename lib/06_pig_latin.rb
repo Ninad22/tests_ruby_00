afd99fac40f2f53e18990ca3aec8bb7f15917101
Lola-D <lola.donval@gmail.com>
2020-01-20 20:56:39 +0100

    second commit

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,8 +1,15 @@
-def translate(str)
-    str << %w(a e i o u).include?(str[0]) ? 'ay' : 'bay'
-        str<<'ay'
-    # else
-    #     str.split.gsub([0])
-    # end
-    return str
+$vowels = "aeiou"
+
+def consonant(s)
+  n = 0
+     while n < s.length
+        if $vowels.include?(s[n]) && s[n-1..n] != "qu"
+           return "#{s[n..-1]}#{s[0..n-1]}ay"
+        else
+           n += 1
+        end
+     end
+  end
+def translate(s)
+    s.split.map{ |s| $vowels.include?(s[0]) ? "#{s}ay" : consonant(s) }.join(" ")
 end
